4765c77c23a571d519169b5f4f8a6db4e0d74e28
Kyung <kbaek95@gmail.com>
2019-02-19 22:05:20 -0500

    Added login endpoint

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -30,7 +30,9 @@ def register():
         elif not password:
             error = "Password is required"
         elif (
-            db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone() is not None:
+            db.execute("SELECT id FROM user WHERE username = ?", (username,)).fetchone()
+            is not None
+        ):
             error = f"User {username} is already registered."
 
         if error is None:
@@ -46,19 +48,28 @@ def register():
     return render_template("auth/register.html")
 
 
+@bp.route("/login", methods=("GET", "POST"))
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
 
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
 
+        if error is None:
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
 
+        flash(error)
 
-
-
-
-
-
-
-
-
-
-
-
+    return render_template("auth/login.html")
 
